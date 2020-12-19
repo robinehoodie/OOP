84438667915156d1094ca46c502e4940e0b0be4b
z3n5hif7 <37139511+z3n5hif7@users.noreply.github.com>
2020-12-19 22:47:30 +0800

    pawn

--- Program-Your-Own-Chess-master/ChessGUI/src/chessgui/pieces/Pawn.java
+++ Program-Your-Own-Chess-master/ChessGUI/src/chessgui/pieces/Pawn.java
@@ -33,8 +33,81 @@ public class Pawn extends Piece {
 
                 // WRITE CODE HERE
         
-                
-                
+        boolean hasMoved = this.getHasMoved();
+        
+    	int x = this.getX();
+    	int y = this.getY();
+        
+    	int distance_x = Math.abs(destination_x - x);    	
+    	int distance_y = Math.abs(destination_y - y);
+    	
+    	
+        Piece p = board.getPiece(destination_x, destination_y);
+        
+        if( p != null) {
+        	if(p.isWhite() && this.isWhite()) {
+        		return false;
+        	}
+        	else if(p.isBlack() && this.isBlack()) {
+        		return false;
+        	}
+        }
+        
+    	String direction = "";
+        
+        if(distance_x > 1 || distance_y > 2) {
+        	return false;
+        }
+    	if(distance_x == 1 && distance_y != 1) {
+    		return false;
+    	}
+    	
+        if(y > destination_y) {
+        	direction = "north";
+        }
+        if(y < destination_y) {
+        	direction = "south";
+        }
+        if(has_moved && distance_y > 1) {
+        	return false;
+        }
+        
+        if("north".equals(direction)) {
+        	if(this.isWhite()) {
+        		return false;
+        	}
+        	if(p == null && distance_x == 1) {
+        		return false;
+        	}
+        	if(distance_x != 1) {
+        		Piece nPiece = board.getPiece(x, y - 1);
+        		Piece nPiece1 = board.getPiece(x, y - 2);
+        		if(nPiece != null) {
+        			return false;
+        		}
+        		if(distance_y == 2 && nPiece1 != null) {
+        			return false;
+        		}
+        	}
+        }
+        if("south".equals(direction)) {
+        	if(this.isBlack()) {
+        		return false;
+       		}
+       		if(p == null && distance_x == 1) {
+           		return false;
+           	}
+           	if(distance_x != 1) {
+           		Piece nPiece = board.getPiece(x, y + 1);
+           		Piece nPiece1 = board.getPiece(x, y + 2);
+           		if(nPiece != null) {
+           			return false;
+           		}
+           		if(distance_y == 2 && nPiece1 != null) {
+           			return false;
+           		}
+        	}
+        }       
         return true;
     }
 }
