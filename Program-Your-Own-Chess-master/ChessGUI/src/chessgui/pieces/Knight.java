ecbb852b5e45be99c494d97ee7cf0341e79f13f4
z3n5hif7 <37139511+z3n5hif7@users.noreply.github.com>
2021-06-27 23:04:33 +0800

    new knight

--- Program-Your-Own-Chess-master/ChessGUI/src/chessgui/pieces/Knight.java
+++ Program-Your-Own-Chess-master/ChessGUI/src/chessgui/pieces/Knight.java
@@ -4,46 +4,36 @@ import chessgui.Board;
 
 public class Knight extends Piece {
 
-    public Knight(int x, int y, boolean is_white, String file_path, Board board, String PIECETYPE, boolean pass)
-    {
+    public Knight(int x, int y, boolean is_white, String file_path, Board board, String PIECETYPE, boolean pass){
         super(x,y,is_white,file_path, board, PIECETYPE, pass);
     }
     
     @Override
-    public boolean canMove(int destination_x, int destination_y)
-    {
-
-        // Remember: a knight can move in any L shape and can jump over anyone
-        // in order to do so. He cannot attack his own pieces.
-        // By an L shape, I mean it can move to a square that is 2 squares away
-        // horizontally and 1 square away vertically, or 1 square away horizontally
-        // and 2 squares away vertically.
-        // some examples:
-        //
-        //  * *       * * *           *       *
-        //  *             *       * * *       *
-        //  *                                 * *
-            
-                // WRITE CODE HERE
+    public boolean canMove(int destination_x, int destination_y){
+        boolean ret=false;
+        
         int x = this.getX();
     	int y = this.getY();
     	
     	int diagonal_y = Math.abs(destination_y - y);
     	int diagonal_x = Math.abs(destination_x - x);
  
-        Piece pPiece = board.getPiece(destination_x, destination_y);
-        if( pPiece != null) {
-        	if(pPiece.isWhite() && this.isWhite()) {
+        Piece p = board.getPiece(destination_x, destination_y);
+        if( p != null) {
+        	if(p.isWhite() && this.isWhite()) {
         		return false;
         	}
-        	else if(pPiece.isBlack() && this.isBlack()) {
+        	else if(p.isBlack() && this.isBlack()) {
         		return false;
         	}
         }
-        
-        if(((diagonal_x == 2 && diagonal_y == 1)||(diagonal_y == 2 && diagonal_x == 1))) {
-        	return true;
+        if(board.validation(destination_x, destination_y,"KNIGHT")){
+            if(((diagonal_x == 2 && diagonal_y == 1)||(diagonal_y == 2 && diagonal_x == 1))) {
+                return true;
+            }
+        }else{
+            return false; 
         }
-        return false; 
+        return ret;
     }
 }
