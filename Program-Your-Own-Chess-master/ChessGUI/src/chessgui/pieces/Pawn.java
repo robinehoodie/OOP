c8125e0a3b36447a660cb34b3881d0e254033d69
z3n5hif7 <37139511+z3n5hif7@users.noreply.github.com>
2021-06-27 23:04:52 +0800

    new pawn

--- Program-Your-Own-Chess-master/ChessGUI/src/chessgui/pieces/Pawn.java
+++ Program-Your-Own-Chess-master/ChessGUI/src/chessgui/pieces/Pawn.java
@@ -6,31 +6,21 @@ public class Pawn extends Piece {
 
     private boolean has_moved;
     
-    public Pawn(int x, int y, boolean is_white, String file_path, Board board, String PIECETYPE, boolean pass)
-    {
+    public Pawn(int x, int y, boolean is_white, String file_path, Board board, String PIECETYPE, boolean pass){
         super(x,y,is_white,file_path, board, PIECETYPE, pass);
         has_moved = false;
     }
     
-    public void setHasMoved(boolean has_moved)
-    {
+    public void setHasMoved(boolean has_moved){
         this.has_moved = has_moved;
     }
     
-    public boolean getHasMoved()
-    {
+    public boolean getHasMoved(){
         return has_moved;
     }
     
     @Override
     public boolean canMove(int destination_x, int destination_y){
-        // Remember: A pawn may only move towards the oponent's side of the board.
-        // If the pawn has not moved yet in the game, for its first move it can 
-        // move two spaces forward. Otherwise, it may only move one space. 
-        // When not attacking it may only move straight ahead.
-        // When attacking it may only move space diagonally forward
-
-                // WRITE CODE HERE
         
         boolean hasMoved = this.getHasMoved();
         
@@ -40,6 +30,7 @@ public class Pawn extends Piece {
 
     	int distance_x = Math.abs(destination_x - x);    	
     	int distance_y = Math.abs(destination_y - y);
+
 
     	
         Piece p = board.getPiece(destination_x, destination_y);
@@ -52,63 +43,70 @@ public class Pawn extends Piece {
         		return false;
         	}
         }
+        if(board.validation(destination_x, destination_y,"PAWN")){
+			int	passantX = destination_x;
+			int passantY = destination_y-1;
 
-    	String direction = "";
-        
-        if(distance_x > 1 || distance_y > 2) {
-        	return false;
-        }
-    	if(distance_x == 1 && distance_y != 1) {
-    		return false;
-    	}
-    	
-        if(y > destination_y) {
-        	direction = "north";
-        }
-        if(y < destination_y) {
-        	direction = "south";
-        }
-        if(has_moved && distance_y > 1) {
-        	return false;
-        }
+			Piece passing = board.getPiece(passantX, passantY);
 
-        if("north".equals(direction)) {
-        	if(this.isBlack()) {
-        		return false;
-        	}
-        	if(p == null && distance_x == 1) {
-        		return false;
-        	}
-        	if(distance_x != 1) {
-        		Piece move = board.getPiece(x, y - 1);
-        		Piece firstMove = board.getPiece(x, y - 2);
-        		if(move != null) {
-        			return false;
-        		}
-        		if(distance_y == 2 && firstMove != null) {
-        			return false;
-        		}
-        	}
-        }
-        if("south".equals(direction)) {
-        	if(this.isWhite()) {
-        		return false;
-       		}
-       		if(p == null && distance_x == 1) {
-           		return false;
-           	}
-           	if(distance_x != 1) {
-           		Piece move = board.getPiece(x, y + 1);
-           		Piece firstMove = board.getPiece(x, y + 2);
-           		if(move != null) {
-           			return false;
-           		}
-           		if(distance_y == 2 && firstMove != null) {
-           			return false;
-           		}
-        	}
-        }       
+			String direction = "";
+			
+			if(distance_x > 1 || distance_y > 2) {
+				return false;
+			}
+			if(distance_x == 1 && distance_y != 1) {
+				return false;
+			}
+			
+			if(y > destination_y) {
+				direction = "north";
+			}
+			if(y < destination_y) {
+				direction = "south";
+			}
+			if(has_moved && distance_y > 1) {
+				return false;
+			}
 
-        return true;
+			if("north".equals(direction)) {
+				if(this.isBlack()) {
+					return false;
+				}
+				if(p == null && distance_x == 1) {
+					return false;
+				}
+				if(distance_x != 1) {
+					Piece move = board.getPiece(x, y - 1);
+					Piece firstMove = board.getPiece(x, y - 2);
+					if(move != null) {
+						return false;
+					}
+					if(distance_y == 2 && firstMove != null) {
+						return false;
+					}
+				}
+			}
+			if("south".equals(direction)) {
+				if(this.isWhite()) {
+					return false;
+				}
+				if(p == null && distance_x == 1) {
+					return false;
+				}
+				if(distance_x != 1) {
+					Piece move = board.getPiece(x, y + 1);
+					Piece firstMove = board.getPiece(x, y + 2);
+					if(move != null) {
+						return false;
+					}
+					if(distance_y == 2 && firstMove != null) {
+						return false;
+					}
+				}
+			}     
+			return true; 
+		}else{
+			return false;
 		}
+	}
 }
