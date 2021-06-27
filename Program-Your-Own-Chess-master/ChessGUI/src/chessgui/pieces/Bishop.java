e654889a818d02697f80893b6bec6769d3853a0b
z3n5hif7 <37139511+z3n5hif7@users.noreply.github.com>
2021-06-27 23:03:51 +0800

    new bishop

--- Program-Your-Own-Chess-master/ChessGUI/src/chessgui/pieces/Bishop.java
+++ Program-Your-Own-Chess-master/ChessGUI/src/chessgui/pieces/Bishop.java
@@ -4,19 +4,13 @@ import chessgui.Board;
 
 public class Bishop extends Piece {
 
-    public Bishop(int x, int y, boolean is_white, String file_path, Board board, String PIECETYPE, boolean pass)
-    {
+    public Bishop(int x, int y, boolean is_white, String file_path, Board board, String PIECETYPE, boolean pass){
         super(x,y,is_white,file_path, board, PIECETYPE,pass);
     }
     
     @Override
-    public boolean canMove(int destination_x, int destination_y)
-    {
-        // Remember: For attacking or just moving, a bishop is allowed to move 
-        // as many squares diagonally as it wants without jumping over another 
-        // piece. He cannot attack his own pieces.
-        
-                // WRITE CODE HERE
+    public boolean canMove(int destination_x, int destination_y){
+
     	int x = this.getX();
     	int y = this.getY();
     	
@@ -37,60 +31,63 @@ public class Bishop extends Piece {
         if(diagonal_x != diagonal_y) {
         	return false;
         }
-        
-        int diagonal_length = diagonal_x;
-         
-        String direction = "";
-        
-        if(destination_y > y && destination_x < x) {
-        	direction = "southwest";
-        }
-        if(destination_y < y && destination_x < x) {
-        	direction = "northwest";
-        }
-        if(destination_y > y && destination_x > x) {
-        	direction = "southeast";
-        }
-        if(destination_y < y && destination_x > x) {
-        	direction = "northeast";
-        }
-         
-        if(direction.equals("southeast")) {
-            for(int i = 1; i < diagonal_length; i++) {
-        	Piece p = board.getPiece(x + i, y + i);
-        	if( p != null) {
-                    return false;
-        	}
+        if(board.validation(destination_x, destination_y,"BISHOP")){      
+            int diagonal_length = diagonal_x;
+            
+            String direction = "";
+            
+            if(destination_y > y && destination_x < x) {
+                direction = "southwest";
             }
-        }
-        
-        if(direction.equals("northeast")) {
-            for(int i = 1; i < diagonal_length; i++) {
-        	Piece p = board.getPiece(x + i, y - i);
-                if( p != null) {
-                    return false;
+            if(destination_y < y && destination_x < x) {
+                direction = "northwest";
+            }
+            if(destination_y > y && destination_x > x) {
+                direction = "southeast";
+            }
+            if(destination_y < y && destination_x > x) {
+                direction = "northeast";
+            }
+            
+            if(direction.equals("southeast")) {
+                for(int i = 1; i < diagonal_length; i++) {
+                Piece p = board.getPiece(x + i, y + i);
+                    if( p != null) {
+                            return false;
+                    }
                 }
             }
-        }
-        
-        if(direction.equals("southwest")) {
-            for(int i = 1; i < diagonal_length; i++) {
-        	Piece p = board.getPiece(x - i, y + i);
-        	if( p != null) {
-                    return false;
-        	}
+            
+            if(direction.equals("northeast")) {
+                for(int i = 1; i < diagonal_length; i++) {
+                Piece p = board.getPiece(x + i, y - i);
+                    if( p != null) {
+                        return false;
+                    }
+                }
             }
-        }
-        
-        if(direction.equals("northwest")) {
-            for(int i = 1; i < diagonal_length; i++) {
-        	Piece p = board.getPiece(x - i, y - i);
-        	if( p != null) {
-                    return false;
-        	}
+            
+            if(direction.equals("southwest")) {
+                for(int i = 1; i < diagonal_length; i++) {
+                Piece p = board.getPiece(x - i, y + i);
+                    if( p != null) {
+                            return false;
+                    }
+                }
             }
+            
+            if(direction.equals("northwest")) {
+                for(int i = 1; i < diagonal_length; i++) {
+                Piece p = board.getPiece(x - i, y - i);
+                    if( p != null) {
+                            return false;
+                    }
+                }
+            }
+            return true;
+        }else{
+            return false;
         }
         
-        return true;
     }
 }
